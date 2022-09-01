ba73d45c7ebb33fa8fed74226248f5117b4a8950
galee1025 <galee1025@gamil.com>
2022-09-01 17:09:17 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/Pages/Gaeul/Login/Login.js
+++ src/Pages/Gaeul/Login/Login.js
@@ -1,7 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Login';
+import './Login.scss';
+import { Link } from 'react-router-dom';
 
 function Login() {
-  return <div>Gaeul Login</div>;
+  const [userId, setUserId] = useState();
+  function saveUserId(event) {
+    setUserId(event.target.value);
+  }
+
+  const [userPw, setUserPw] = useState();
+  function saveUserPw(event) {
+    setUserPw(event.target.value);
+  }
+
+  return (
+    <>
+      <section className="main-box">
+        <div className="logo">Westagram</div>
+
+        <form className="wrap">
+          <input
+            type="text"
+            className="nameEmail"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+            onChange={saveUserId}
+          />
+          <input
+            type="password"
+            className="passWord"
+            placeholder="비밀번호"
+            onChange={saveUserPw}
+          />
+          <button className="login-btn">
+            <Link to="/maing">로그인</Link>
+          </button>
+        </form>
+
+        <div className="or-wrap">
+          <div className="line" />
+          <p>또는</p>
+          <div className="line" />
+        </div>
+
+        <div className="facebook-login-btn">
+          <button>
+            <img src="images/Gaeul/facebook_icon.png" />
+            <span>Facebook으로 로그인</span>
+          </button>
+        </div>
+
+        <div className="forgot-password">
+          <a href="#">비밀번호를 잊으셨나요?</a>
+        </div>
+      </section>
+
+      <section className="join-box">
+        <p className="let-join">
+          계정이 없으신가요?<a href="#">가입하기</a>
+        </p>
+      </section>
+
+      <div className="app-down">
+        <p>앱을 다운로드하세요.</p>
+        <div className="download-image">
+          <a href="https://apps.apple.com/app/instagram/id389801252?vt=lo">
+            <img src="images/Gaeul/appstoredownload.png" />
+          </a>
+          <a
+            href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DloginPage%26ig_mid%3D9B98617F-78AA-471A-A22F-E8F80A1366E9%26utm_content%3Dlo%26utm_medium%3Dbadge"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src="images/Gaeul/googleplaydownload.png" />
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;
--- src/Pages/Gaeul/Main/Main.js
+++ src/Pages/Gaeul/Main/Main.js
@@ -1,7 +1,257 @@
 import React from 'react';
+import './Main';
+import './Main.scss';
 
 function Main() {
-  return <div>Gaeul Main</div>;
+  return (
+    <>
+      <header>
+        <div className="modal-bg" />
+        <div className="header-wrap">
+          <div className="logo-wrap">
+            <img className="insta-logo" src="images/Gaeul/instagram.png" />
+            <div className="logo-line" />
+            <div className="logo-text">Westagram</div>
+          </div>
+
+          <form className="search-wrap">
+            <span id="search-icon">
+              <i className="fa-solid fa-magnifying-glass" />
+            </span>
+            <input type="text" className="search-bar" placeholder="검색" />
+          </form>
+
+          <div className="icons">
+            <button>
+              <img
+                className="icon-explore"
+                src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
+              />
+            </button>
+
+            <button>
+              <img
+                className="icon-heart"
+                src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
+              />
+            </button>
+
+            <button className="icon-mypage">
+              <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png" />
+            </button>
+
+            <div className="modal hidden">
+              <div className="modal-content">
+                <a>
+                  <i className="fa-regular fa-circle-user" />
+                  프로필
+                </a>
+                <a>
+                  <i className="fa-regular fa-bookmark" />
+                  저장됨
+                </a>
+                <a href="#">
+                  <i className="fa-solid fa-gear" />
+                  설정
+                </a>
+                <a>로그아웃</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <section className="main">
+        <div className="feeds">
+          <div className="article-wrap">
+            <div className="user-bar">
+              <div className="user">
+                <div className="user-image" />
+                <p className="user-id">galee326</p>
+              </div>
+
+              <button id="dot">
+                <i class="fa-solid fa-ellipsis" />
+              </button>
+            </div>
+
+            <div className="article-img" />
+
+            <div className="article-icons">
+              <div className="article-three-icons">
+                <button>
+                  <i className="fa-regular fa-heart" />
+                </button>
+                <button>
+                  <i className="fa-regular fa-comment" />
+                </button>
+                <button>
+                  <i className="fa-solid fa-arrow-up-from-bracket" />
+                </button>
+              </div>
+
+              <button className="bookmark">
+                <i class="fa-regular fa-bookmark" />
+              </button>
+            </div>
+
+            <div className="like-user">
+              <div className="like-user-img" />
+              <p>
+                <b>everyone_woo</b>님 외 <b>12명</b>이 좋아합니다
+              </p>
+            </div>
+
+            <ul className="comments-ul">
+              <li className="comment-li">
+                <span className="comment-text">
+                  <em>somebody</em>&nbsp;&nbsp;케이크 좋아
+                </span>
+                <div clasNames="comment-btn-wrap">
+                  <button className="comment-heart-btn">
+                    <i class="fa-regular fa-heart" />
+                  </button>
+                  <button className="delete-btn">
+                    <i className="fa-solid fa-trash-can" />
+                  </button>
+                </div>
+              </li>
+            </ul>
+
+            <form className="lets-comment">
+              <input
+                className="comment-input"
+                type="text"
+                placeholder="댓글 달기..."
+              />
+              <button className="comment-button">게시</button>
+            </form>
+          </div>
+        </div>
+
+        <div className="main-right">
+          <div className="id-info-box">
+            <div className="my-image" />
+            <div className="my-info">
+              <p id="my-id">wecode_bootcamp</p>
+              <p id="my-id-text">WeCode | 위코드</p>
+            </div>
+          </div>
+
+          <section className="story">
+            <div className="story-bar">
+              <h5>스토리</h5>
+              <button onClick="">모두 보기</button>
+            </div>
+
+            <div className="story-user">
+              <div className="user-image" />
+              <div className="user-info">
+                <p className="user-id">user001</p>
+                <p className="timeline">16분 전</p>
+              </div>
+            </div>
+
+            <div className="story-user">
+              <div className="user-image" />
+              <div className="user-info">
+                <p className="user-id">user002</p>
+                <p className="timeline">20분 전</p>
+              </div>
+            </div>
+
+            <div className="story-user">
+              <div className="user-image" />
+              <div className="user-info">
+                <p className="user-id">user003</p>
+                <p className="timeline">22분 전</p>
+              </div>
+            </div>
+
+            <div className="story-user">
+              <div className="user-image" />
+              <div className="user-info">
+                <p className="user-id">user004</p>
+                <p className="timeline">36분 전</p>
+              </div>
+            </div>
+
+            <div className="story-user">
+              <div className="user-image" />
+              <div className="user-info">
+                <p className="user-id">user005</p>
+                <p className="timeline">40분 전</p>
+              </div>
+            </div>
+          </section>
+
+          <section className="recommend">
+            <div className="story-bar">
+              <h5>회원님을 위한 추천</h5>
+              <button onClick="">모두 보기</button>
+            </div>
+
+            <div className="story-recommend-user">
+              <div className="user-image" />
+              <div className="user-info">
+                <p className="user-id">user_1</p>
+                <p className="recommend-text">canon_mj님 외 2명이 팔로우</p>
+              </div>
+              <button className="follow" href="">
+                팔로우
+              </button>
+            </div>
+
+            <div className="story-recommend-user">
+              <div className="user-image" />
+              <div className="user-info">
+                <p className="user-id">user_2</p>
+                <p className="recommend-text">mama_h님 외 5명이 팔로우</p>
+              </div>
+              <button className="follow">팔로우</button>
+            </div>
+
+            <div className="story-recommend-user">
+              <div className="user-image" />
+              <div className="user-info">
+                <p className="user-id">user_3</p>
+                <p className="recommend-text">m_amah님 외 2명이 팔로우</p>
+              </div>
+              <button className="follow">팔로우</button>
+            </div>
+
+            <div className="story-recommend-user">
+              <div className="user-image" />
+              <div className="user-info">
+                <p className="user-id">user_4</p>
+                <p className="recommend-text">mama_h님 외 5명이 팔로우</p>
+              </div>
+              <button className="follow">팔로우</button>
+            </div>
+
+            <div className="story-recommend-user">
+              <div className="user-image" />
+              <div className="user-info">
+                <p className="user-id">user_4</p>
+                <p className="recommend-text">mama_h님 외 5명이 팔로우</p>
+              </div>
+              <button className="follow">팔로우</button>
+            </div>
+          </section>
+
+          <footer>
+            <div className="site-info">
+              <p>
+                소개 · 도움말 · 홍보 센터 · API · 채용 정보 · 개인정보처리방침 ·
+                약관 · 위치 · 언어
+              </p>
+              <p>© 2022 INSsGRAM FROM META</p>
+            </div>
+          </footer>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Main;
